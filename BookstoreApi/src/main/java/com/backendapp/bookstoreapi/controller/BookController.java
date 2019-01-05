ad1f0999ea1c921630d32bd40b08fd9f01c1e8bd
SAHAR BT <SAHAR BT@192.168.118.1>
2019-01-05 17:07:56 +0100

    Update Book Controller: generate CRUD operations

--- BookstoreApi/src/main/java/com/backendapp/bookstoreapi/controller/BookController.java
+++ BookstoreApi/src/main/java/com/backendapp/bookstoreapi/controller/BookController.java
@@ -1,14 +1,29 @@
 package com.backendapp.bookstoreapi.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.backendapp.bookstoreapi.model.Book;
 import com.backendapp.bookstoreapi.service.BookService;
@@ -17,6 +32,8 @@ import com.backendapp.bookstoreapi.service.BookService;
 @RequestMapping("/book")
 public class BookController {
 
+	private String imageName;
+	
 	@Autowired
 	private BookService service;
 	
@@ -25,13 +42,81 @@ public class BookController {
 		return service.save(book);
 	}
 	
+	@RequestMapping(value="/add/image", method=RequestMethod.POST)
+	public ResponseEntity upload(@RequestParam("id") Long id,
+			HttpServletResponse response, HttpServletRequest request) {
+		try {
+			Book book = service.findById(id);
+			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+			Iterator<String> it = multipartRequest.getFileNames();
+			MultipartFile multipartFile = multipartRequest.getFile(it.next());
+			
+			String fileName = id+".png";
+			imageName = fileName;
+			
+			byte[] bytes = multipartFile.getBytes();
+			BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File("src/main/resources/static/image/book/"+fileName)));
+			stream.write(bytes);
+			stream.close();
+			return new ResponseEntity("upload success!", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity("upload failed!", HttpStatus.BAD_REQUEST);
+	}
+	
+	@RequestMapping(value="/update/image", method=RequestMethod.POST)
+	public ResponseEntity updateImagePost(@RequestParam("id") Long id,
+			HttpServletResponse response, HttpServletRequest request) {
+		try {
+			Book book = service.findById(id);
+			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+			Iterator<String> it = multipartRequest.getFileNames();
+			MultipartFile multipartFile = multipartRequest.getFile(it.next());
+			
+			String fileName = id+".png";
+			imageName = fileName;
+			
+			if (Paths.get("src/main/resources/static/image/book/"+fileName) != null)
+			Files.delete(Paths.get("src/main/resources/static/image/book/"+fileName));
+			
+			byte[] bytes = multipartFile.getBytes();
+			BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File("src/main/resources/static/image/book/"+fileName)));
+			stream.write(bytes);
+			stream.close();
+			return new ResponseEntity("upload success!", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity("upload failed!", HttpStatus.BAD_REQUEST);
+	}
+	
+	@RequestMapping("/bookList")
+	public List<Book> getBookList() {
+		return service.findAll();
+	}
+	
 	@GetMapping("/getBooks")
 	public List<Book> getBooks() {
 		return service.findAll();
 	}
+	@RequestMapping(value="/update", method=RequestMethod.POST)
+	public Book updateBookPost(@RequestBody Book book) {
+		return service.save(book);
+	}
 	
-	@RequestMapping(path="/{title}")
-	public List<Book> getBookByName(@PathVariable("title") String title) {
-		return service.findByTitle(title);
+	@RequestMapping(value="/remove", method=RequestMethod.POST)
+	public ResponseEntity remove(@RequestBody String id) throws IOException {
+		service.delete(Long.parseLong(id));
+		
+		String fileName = id+".png";
+		Files.delete(Paths.get("src/main/resources/static/image/book/"+fileName));
+		
+		return new ResponseEntity("remove success!", HttpStatus.OK);
+	}
+	
+	@RequestMapping(path="/{id}")
+	public Book getBook(@PathVariable("id") Long id) {
+		return service.findById(id);
 	}
 }
